5d7e40c5f89597424757ba942b347d6699650144
Testerek123 <lh.seller1.1.test@gmail.com>
2023-08-14 11:00:08 +0200

    v1.1

---
+++ src/main/java/Moon.java
@@ -0,0 +1,9 @@
+public class Moon {
+    public static void main(String[] args) {
+        String word = "Hello guys :), welcom in GitHub World! \nŁukasz";
+
+        System.out.println(word);
+        System.out.println("------------------------------");
+
+    }
+}
