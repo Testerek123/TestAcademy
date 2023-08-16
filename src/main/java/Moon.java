e0acc2efe07f98453f412608d16f53b457da84c9
Testerek456 <lukasz.hagel@gmail.com>
2023-08-16 15:50:56 +0200

    Lukasz answer update

--- src/main/java/Moon.java
+++
@@ -1,9 +0,0 @@
-public class Moon {
-    public static void main(String[] args) {
-        String word = "Hello guys :), welcom in GitHub World! \nŁukasz";
-
-        System.out.println(word);
-        System.out.println("------------------------------");
-
-    }
-}
---
+++ src/main/java/answers/lukasz/Task1/Task1_Days.java
@@ -0,0 +1,10 @@
+package answers.lukasz.Task1;
+
+public class Task1_Days {
+
+    enum DayOfWeek {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY}
+
+    //2nd example
+    enum MonthsOfTheYear {JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER}
+
+}
