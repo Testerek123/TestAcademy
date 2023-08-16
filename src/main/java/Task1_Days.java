ef96ad90beefacbfc0a72157d0a7af4700229338
Testerek456 <lukasz.hagel@gmail.com>
2023-08-16 11:47:27 +0200

    Answer to task 1

---
+++ src/main/java/Task1_Days.java
@@ -0,0 +1,12 @@
+public class Task1_Days {
+
+    public enum DayOfWeek {SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY}
+
+    public static void main(String[] args) {
+        for (DayOfWeek days : DayOfWeek.values()){
+            System.out.println(days);
+        }
+    }
+
+
+}
