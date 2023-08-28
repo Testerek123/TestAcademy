5405a50a4d3c65199c35c6ae9aeba8a661968494
laxmikoladur11 <koladur.laxmi@gmail.com>
2023-08-28 13:12:02 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/answers/laxmi/Basicjava.java
@@ -0,0 +1,21 @@
+package answers.laxmi;
+import java.util.Scanner;
+
+public class Basicjava {
+
+    public static void main (String[] args)
+    {
+        Scanner scann= new Scanner(System.in);
+        System.out.println("enter string");
+       // String s1 = scann.next();
+        //System.out.println(s1. + "djhsfjhf");
+        String s2 = scann.nextLine();
+
+
+
+
+
+    }
+
+}
+
---
+++ src/main/java/answers/laxmi/Methodarguments.java
@@ -0,0 +1,13 @@
+package answers.laxmi;
+
+public class Methodarguments {
+
+    static void methoda(String name)
+    {
+        System.out.println(name +"njd");
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
