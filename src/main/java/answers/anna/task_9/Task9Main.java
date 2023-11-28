0d83daf5a6d8fe541a2cf80a308b5062afd4020e
Anna Dutkowiak <Anna.Dutkowiak+EYGS@gds.ey.com>
2023-11-28 10:15:45 +0100

    Anna's task 9 solution update 28/11/2023

--- src/main/java/answers/anna/task_9/Task9Main.java
+++ src/main/java/answers/anna/task_9/Task9Main.java
@@ -1,10 +1,32 @@
 package answers.anna.task_9;
+import java.util.Scanner;
 
 public class Task9Main {
 
     public static void main(String[] args){
-        snailTrip(10, 3, 2);
+        //snailTrip(10, 3, 2);
+        getDataFromUser();
 
+    }
+
+    public static void getDataFromUser(){
+        Scanner sc = new Scanner(System.in);
+        int pole = 0;
+        int day = 0;
+        int night = 0;
+        System.out.println("Provide values for snails journey(all values has to be greater then 0):\s");
+        System.out.println("Lenght of the pole:\s");
+        pole = sc.nextInt();
+        System.out.println("Daily progress:\s");
+        day = sc.nextInt();
+        System.out.println("Nightly regress:\s");
+        night = sc.nextInt();
+        if (pole >= 0 && day > night && day >=0 && night >=0){
+            snailTrip(pole, day, night);
+        } else {
+            System.out.println("incorrect data, try again");
+            getDataFromUser();
+        }
     }
 
     public static void snailTrip(int pole, int day, int night ){
